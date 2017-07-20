b252df3b43f2c10f4f936638c98e63836a2c6802
Viacheslav Mefodin <mefodin.v@gmail.com>
2017-07-21 00:08:53 +0300

    do not delete devise omniauthable module

--- app/models/devise_token_auth/concerns/user.rb
+++ app/models/devise_token_auth/concerns/user.rb
@@ -20,7 +20,7 @@ module DeviseTokenAuth::Concerns::User
       devise :database_authenticatable, :registerable,
           :recoverable, :trackable, :validatable, :confirmable
     else
-      self.devise_modules.delete(:omniauthable)
+      #self.devise_modules.delete(:omniauthable)
     end
 
     unless tokens_has_json_column_type?
